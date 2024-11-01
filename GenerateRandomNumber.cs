54a7fecf24fd86a551b4fbe26cdd1dfb0392867c
Fernando Gómez <141048801+Axus00@users.noreply.github.com>
2024-11-01 18:10:27 -0500

    Add files via upload

---
+++ GenerateRandomNumber.cs
@@ -0,0 +1,12 @@
+﻿using System.Net;
+
+namespace RandomNumber;
+
+public class GenerateRandomNumber
+{
+    public static int GenerateNumber()
+    {
+        Random random = new Random();
+        return random.Next(1000, 10000);
+    }
+}
